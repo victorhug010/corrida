71d93f8ed9897aca0023f5681098b41497d64daa
victor <victor.hug0@outlook.com>
2022-05-27 03:02:21 -0300

    segundamudança

--- projetocorrida/src/app/app-routing.module.ts
+++ projetocorrida/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PistaComponent } from './components/pista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'corrida', component: PistaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
